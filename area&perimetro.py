23af5f5be90ddcce270e197b63540f55c6c07180
Daniel Araújo <contatodanielaraujo@hotmail.com>
2025-03-31 11:07:41 -0300

    Cálculo para área e perímetro
    Cálculo para colher os m² total da área e os m do perímetro.

---
+++ area&perimetro.py
@@ -0,0 +1,10 @@
+
+print("Calcule a área e o perímetro", "\n")
+
+comprimento = float(input("Digite o Comprimento:"))
+largura = float(input("Digite a Largura:"))
+x = comprimento * largura 
+y = 2*(comprimento + largura)
+print("Calculando...")
+print("Área total m²:", x)
+print("Perímetro total em m:", y)
